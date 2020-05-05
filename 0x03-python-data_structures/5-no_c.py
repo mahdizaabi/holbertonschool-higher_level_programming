e6b0ae32ff8e31e7f4fcc61247407e4c433e096c
Username <mahdi.zaabi@gmail.com>
2020-05-05 17:21:07 +0000

    adding_file

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    listed_string = list(my_string)
-    for s in range(len(my_string)):
-        if listed_string[s] == 'c' or listed_string[s] == 'C':
-            listed_string[s] = ""
-    return(''.join(listed_string))
+    copy = [l for l in my_string if l != 'c' and l != 'C']
+    return ("".join(copy))
