4f95a27af483532288a4443478dd8596a25870fe
Username <mahdi.zaabi@gmail.com>
2020-05-05 05:54:30 +0000

    adding_file

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    return(my_list.index(idx))
+    if idx >= 0 and idx < len(my_list):
+        return(my_list[idx])
+    else:
+        return (None)
