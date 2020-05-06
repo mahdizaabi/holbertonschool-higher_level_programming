c6efd7630913449cc4196a0cf9ec56e2b3d1d57e
Username <mahdi.zaabi@gmail.com>
2020-05-06 04:28:09 +0000

    adding_file

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list == 0:
+        return None
+    for x in my_list:
+        if my_list[0] < x:
+            c = x
+        return(c)
+    
