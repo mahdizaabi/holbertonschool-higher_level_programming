b4987fe8012d40f3d6c92553a188306a2ca36aa9
Username <mahdi.zaabi@gmail.com>
2020-05-07 18:06:45 +0000

    adding_file

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    summe_1 = 0
+    for tuple in my_list:
+        summe1 += (tuple[0] * tuple[1])
+    summe_2 = 0
+    for tuple in my_list:
+        summe_2 += tuple[1]
+    return (summe_1 / summe_2)
