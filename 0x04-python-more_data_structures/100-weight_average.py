accbe5cc88906845778f37277cfb94978a126f2d
Username <mahdi.zaabi@gmail.com>
2020-05-08 07:13:18 +0000

    adding_file

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -9,3 +9,9 @@ def weight_average(my_list=[]):
     for x, j in my_list:
         listy.append(j)
     return(sum(listix)/sum(listy))
+
+
+           #second methods :
+       #liste comprehension : 
+   #somme = sum([x*y for x, y in my_list])
+  #return(somme/sum([y for x, y in my_list])
