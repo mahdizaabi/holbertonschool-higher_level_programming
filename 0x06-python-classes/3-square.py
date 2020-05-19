0cd9adfba2aabf45c0a10201e1ceca91b9a562be
mahdizaabi <mahdi.zaabi@gmail.com>
2020-05-19 21:01:44 +0000

    adding_file

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
 """
-define a class Square
+    documentation of the module square
+         1 - Square
 """
-
-
 class Square:
-    """sqaure class
+    """
+    documentation of the class Square
+        *methodes:
+            __init__
+            area
+        *attributes:
+            size: private
     """
     def __init__(self, size=0):
-
-        """square instance"""
-
+        """documentation of the method __init__: constructor"""
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-        def area(self):
-            """Instance method to calculate the area"""
-
+    def area(self):
+        """documentation of the method area: return the area of square"""
         return self.__size**2
