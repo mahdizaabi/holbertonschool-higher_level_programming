a36d0a0b419ef4b8ec0002fe4cde0155fd84fad5
mahdizaabi <mahdi.zaabi@gmail.com>
2020-05-19 14:57:56 +0000

    adding_file

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 class Square:
     """The summary line for a class docstring should fit on one line
+
     Attributes:
         __size (int): size of square"""
 
@@ -12,4 +13,5 @@ class Square:
             size (int): size of square
         Returns: None
         """
+
         self.__size = size
