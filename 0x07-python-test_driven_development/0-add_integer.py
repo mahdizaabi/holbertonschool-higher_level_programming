fb8ce3fcbc979b9f592787edd895b6a41c151dd9
mahdizaabi <mahdi.zaabi@gmail.com>
2020-05-30 12:22:24 +0000

    adding_file

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/Python3
+'''
+Integer/float addition Module
+'''
+
+
+def add_integer(a, b):
+    '''
+        check type and make operation
+    '''
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    return a + b
