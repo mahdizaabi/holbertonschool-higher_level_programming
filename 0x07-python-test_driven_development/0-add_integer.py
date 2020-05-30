7d7c483b34609264f5235f927eb00601aea8cd3d
mahdizaabi <mahdi.zaabi@gmail.com>
2020-05-30 13:31:21 +0000

    adding_file

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -9,12 +9,9 @@ def add_integer(a, b=98):
         check type and make operation
     '''
 
-    if a % 1 != 0 or (type(a) is not int and type(a) is not float):
-        raise TypeError("a must be an integer")
-    if b % 1 != 0 or (type(b) is not int and type(b) is not float):
-        raise TypeError("b must be an integer")
-    if type(a) is float:
-        a = int(a)
-    if type(b) is float:
-        b = int(b)
-    return a + b
+    if a is None or (type(a) is not int and type(a) is not float):
+        raise TypeError('a must be an integer')
+    if type(b) is not int and type(b) is not float:
+        raise TypeError('b must be an integer')
+    return int(a) + int(b)
+
