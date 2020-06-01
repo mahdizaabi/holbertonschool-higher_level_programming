f6bf7428a84a48fb2178c13e9ec831a914f06ebe
mahdizaabi <mahdi.zaabi@gmail.com>
2020-06-01 03:43:51 +0000

    adding_file

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -9,7 +9,7 @@ def matrix_divided(matrix, div):
     """Return a list that contain all elm of matrix divided"""
 
     if type(matrix) is not list:
-        raise TypeError("matrix must be a matrix (list of lists)\
+        raise TypeError("matrix must be a matrix (list of lists) \
 of integers/floats")
     if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
@@ -17,13 +17,13 @@ of integers/floats")
         raise ValueError("division by zero")
     for elm in matrix:
         if type(elm) is not list:
-            raise TypeError("matrix must be a matrix (list of lists)\
+            raise TypeError("matrix must be a matrix (list of lists) \
 of integers/floats")
         if len(elm) != len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
-
         for i in elm:
             if type(i) is not int and float:
-                raise TypeError("matrix must be a matrix (list of lists)\
+                raise TypeError("matrix must be a matrix (list of lists) \
 of integers/floats")
+
     return([list(map(lambda x: round(x/div, 2), row)) for row in matrix])
