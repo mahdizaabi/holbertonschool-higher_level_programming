330ffad29019d0e399a798ad5cb2fc2d32abd6cd
mahdizaabi <mahdi.zaabi@gmail.com>
2020-06-02 22:36:09 +0000

    addnig_file

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,20 +2,7 @@
 """
 cclass BaseGeometry
 """
-
-
-class BaseGeometry:
-    """A class with public attribute area"""
-    def area(self):
-        """raise Exception"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """validates that value is an integer greater than 0"""
-        if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
