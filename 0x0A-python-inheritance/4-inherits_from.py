8a5f9facc17f7a26091a4a905b356c94dfd7c22a
mahdizaabi <mahdi.zaabi@gmail.com>
2020-06-02 21:24:47 +0000

    adding_file

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,4 +6,4 @@ Check for subclass
 
 def inherits_from(obj, a_class):
     """returns true if obj is a subclass of a_class"""
-    return(issubclass(type(obj), a_class) and type(obj) != a_class)
+    return(issubclass(type(obj), a_class))
