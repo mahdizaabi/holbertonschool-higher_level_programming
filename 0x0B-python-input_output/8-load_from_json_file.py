9bd3120a6dbb20f7f7aacaed7ba259d7d361d081
mahdizaabi <mahdi.zaabi@gmail.com>
2020-06-03 20:54:35 +0000

    adding_file

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -6,7 +6,6 @@ import json
 """import JSON Module"""
 
 
-
 def load_from_json_file(filename):
     """
     Load from Json and write to a file
@@ -14,5 +13,3 @@ def load_from_json_file(filename):
 
     with open(filename, 'r', encoding='utf8') as file:
         return json.load(file)
-    
-    
