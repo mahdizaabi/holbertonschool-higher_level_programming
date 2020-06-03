f5382c6996212ebf7cce6e9c18de0d7137b6c127
mahdizaabi <mahdi.zaabi@gmail.com>
2020-06-03 19:47:14 +0000

    adding_file

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """
-
+write to file
 """
 
 
 def write_file(filename="", text=""):
     """
-
+    write to file
     """
+
+
     with open(filename, 'w', encoding='utf8') as file:
-        return(file.write(text))
-    
-       
+        n = file.write(text)
+    return(n)       
