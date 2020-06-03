7fda69ebc7568ec782ab63f3ccc3910ee5e8e23e
mahdizaabi <mahdi.zaabi@gmail.com>
2020-06-03 18:56:09 +0000

    adding_file

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -11,6 +11,5 @@ def number_of_lines(filename=""):
 
     with open(filename, 'r', encoding="utf-8") as filex:
         for i in filex:
-            print(i, end='')
             g += 1
         return g
