09d50b5c7ee92b082a5530837cd6b0e5c06e82a2
mahdizaabi <mahdi.zaabi@gmail.com>
2020-06-03 18:50:38 +0000

    addin

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,5 +8,5 @@ def read_file(filename=""):
 
     """
 
-    with open(filename, encoding='utf-8') as file:
+    with open(filename, "r", encoding='utf-8') as file:
         print(file.read(), end='')
