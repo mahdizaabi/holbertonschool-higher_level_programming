9ab3125814dfb924675d6a750ff961deed14817e
mahdizaabi <mahdi.zaabi@gmail.com>
2020-06-03 20:11:29 +0000

    adding_file

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """
-
+append text
 """
 
 
 def append_write(filename="", text=""):
     """
-
+    Append text
     """
     with open(filename, 'a', encoding='utf8') as file:
-        return(file.write(text))
-    
-       
+        n = file.write(text)
+    return n
