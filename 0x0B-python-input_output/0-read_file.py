bb42cc4bf2f99d037216335b35627e05c7c61324
mahdizaabi <mahdi.zaabi@gmail.com>
2020-06-03 18:52:07 +0000

    adding_file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-
+Contains the read_file function
 """
 
 def read_file(filename=""):
     """
-
+    Read a etxt file
     """
 
-    with open(filename, "r", encoding='utf-8') as file:
-        print(file.read(), end='')
+    with open(filename, "r", encoding='utf-8') as filex:
+        print(filex.read(), end='')
