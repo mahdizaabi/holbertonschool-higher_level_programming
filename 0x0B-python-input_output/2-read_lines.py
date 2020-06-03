276d142ec33d841ce890c6dc29f220d32e040a80
mahdizaabi <mahdi.zaabi@gmail.com>
2020-06-03 19:15:32 +0000

    adding_file

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -7,16 +7,11 @@ def read_lines(filename="", nb_lines=0):
     """
     """
 
-    g = 0
-    c = 1
+
     with open(filename, 'r', encoding='utf-8') as fx:
-        for i in fx:
-            g += 1
-        fx.seek(0)
-        if nb_lines <= 0 or nb_lines >= g:
-            print(fx.read(), end='')
+        if nb_lines <= 0:
+            for ln in fx:
+                print(ln)
         else:
-            for d in fx:
-                if c <= nb_lines:
-                    print(d, end='');
-                    c += 1
+            for ln in range(nb_lines):
+                    print(fx.readline(), end='')
