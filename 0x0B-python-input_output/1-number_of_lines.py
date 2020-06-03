4035ae222eb8e38911b68a805567e7c362399417
mahdizaabi <mahdi.zaabi@gmail.com>
2020-06-03 19:00:17 +0000

    adding_file

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,15 +1,14 @@
 #!/usr/bin/pyhton3
 """
+Nb of lines
 """
 
 
 def number_of_lines(filename=""):
-    """
-    """
+    """nb of lines"""
 
     g = 0
-
-    with open(filename, 'r', encoding="utf8") as filex:
+    with open(filename, 'r', encoding='utf8') as filex:
         for i in filex:
             g += 1
         return g
