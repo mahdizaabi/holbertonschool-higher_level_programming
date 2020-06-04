006d93f156daf02aa1905302be7e8f3a762232cb
mahdizaabi <mahdi.zaabi@gmail.com>
2020-06-04 11:15:33 +0000

    adding_file

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/pyhton3
+"""
+
+"""
+
+
+class Student:
+    """student attributs"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes the student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """returns a dictionary representation of a Student instance"""
+        return self.__dict__
+
+    def to_json(self, attrs=None):
+
+        new_dic = {}
+        if attrs:
+            for att in attrs:
+                if att in self.__dict__.keys():
+                    new_dic[att] = self.__dict__[att]
+            return new_dic
+        else:
+            return self.__dict__
