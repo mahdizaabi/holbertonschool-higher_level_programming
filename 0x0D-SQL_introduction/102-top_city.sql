6eebe2a904cfb450a957b9f8bb30fc367fd8499c
mahdizaabi <mahdi.zaabi@gmail.com>
2020-07-13 22:28:40 +0000

    adding_file

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,3 +1,3 @@
 -- Displays the top 3 of cities temperature during July and August
 
-SELECT city, AVG(value) AS avg_temp FROM temperatures UP BY city ORDER BY avg_temp DESC LIMIT 3;
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
