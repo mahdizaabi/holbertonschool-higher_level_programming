b070350429da18cbd1242a03f9387536aef23c6d
mahdizaabi <mahdi.zaabi@gmail.com>
2020-07-13 16:49:38 +0000

    adding_file

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,4 +1,4 @@
 -- Select all scores h-e than 10 and display them with the name .
 -- record should be ordered by score >= 10 .
 
-SELECT `score`, `name` FROM second_table where `score` >= 10;
+SELECT `score`, `name` FROM second_table where `score` >= 10 ORDER BY score DESC;
