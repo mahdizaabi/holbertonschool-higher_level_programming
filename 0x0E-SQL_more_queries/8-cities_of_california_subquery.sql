e19cde67069c050d71d5d39d454ba2902848515a
mahdizaabi <mahdi.zaabi@gmail.com>
2020-07-14 15:46:49 +0000

    adding_file

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- lists all the cities of California that can be found in the database
-
+-- use of subquery to retrieve the id corresponding to the table(state)s
 SELECT id, name
 FROM cities
 WHERE state_id = (SELECT ID FROM states WHERE name='California')
