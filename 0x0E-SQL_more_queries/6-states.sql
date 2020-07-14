f76585e4edac76d12c1e1ba2d14c19caf3077cfe
mahdizaabi <mahdi.zaabi@gmail.com>
2020-07-14 14:18:20 +0000

    adding_file

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
--- creates the database hbtn_0d_usa and the table states
+-- creates database and table auto incremented PK id;
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states
-(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
+(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
