6c0602c0ee518b4b66b2d2d62c37ea4771743c50
mahdizaabi <mahdi.zaabi@gmail.com>
2020-07-14 17:01:03 +0000

    adding_file

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- lists all cities contained in the database
 
-SELECT cities.id, citites.name, states.name
+SELECT cities.id, cities.name, states.name
 FROM cities
 JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC;
