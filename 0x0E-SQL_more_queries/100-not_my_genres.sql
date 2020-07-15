b22bbc17ef9840977ac5b8a90fb2ade1e878b18f
mahdizaabi <mahdi.zaabi@gmail.com>
2020-07-15 01:59:20 +0000

    adding_file

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,16 @@
+
+
+
+SELECT name 
+FROM tv_genres
+WHERE tv_genres.id not in(
+
+SELECT tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter'
+)
+ORDER BY tv_genres.name;
+
+
