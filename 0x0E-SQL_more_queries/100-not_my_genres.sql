1a63b45d1ad64f52550576b7e86e6cda6af8a3e5
crxs10 <59511107+mahdizaabi@users.noreply.github.com>
2020-07-29 02:50:36 +0200

    Update 100-not_my_genres.sql

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -13,3 +13,15 @@ WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name;
 
 
+-- SQL                                        
+-- SQL                                        
+                                              
+                                              
+--SELECT name                                   
+--FROM tv_genres where id not in(               
+--select tv_show_genres.genre_id                
+--from tv_shows join tv_show_genres             
+--on tv_show_genres.show_id = tv_shows.id       
+--where (title = 'Dexter'))                     
+--ORDER BY name;                                
+                                              
