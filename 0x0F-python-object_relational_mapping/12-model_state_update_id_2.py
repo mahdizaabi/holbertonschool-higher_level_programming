e377d8da42404ba155f5608f69a661e2fd1a841e
mahdizaabi <mahdi.zaabi@gmail.com>
2020-08-08 20:57:15 +0000

    adding_file

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -10,7 +10,7 @@ from sys import argv
 from model_state import Base, State
 
 if __name__ == "__main__":
-    ziel = argv[4]
+
     eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
                                                                     argv[2],
                                                                     argv[3]))
@@ -20,5 +20,5 @@ if __name__ == "__main__":
     res = session.query(State).get(2)
     res.name = 'New Mexico'
     session.commit()
-
+    res = session.query(State).first()
     session.close()
