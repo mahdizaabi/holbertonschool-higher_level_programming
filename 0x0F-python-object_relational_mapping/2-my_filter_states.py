ed889f8076deec9424381ab1775016441fe17a9f
mahdizaabi <mahdi.zaabi@gmail.com>
2020-08-08 12:25:49 +0000

    adding_file

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -17,6 +17,7 @@ if __name__ == "__main__":
                 .format(statex))
     rows = cur.fetchall()
     for row in rows:
-        print(row)
+        if row[1] == statex:
+            print(row)
     cur.close()
     db.close()
