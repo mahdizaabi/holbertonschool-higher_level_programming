22885f4a704c16d14aa15f0418881645dd9b8bdc
mahdizaabi <mahdi.zaabi@gmail.com>
2020-08-13 01:29:24 +0000

    adding_file

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
                                                                     argv[2],
                                                                     argv[3]))
-    #Base.metadata.create_all(eng)
+    Base.metadata.create_all(eng)
     Session = sessionmaker(bind=eng)
     session = Session()
     res = session.query(State).filter(State.name == ziel).first()
