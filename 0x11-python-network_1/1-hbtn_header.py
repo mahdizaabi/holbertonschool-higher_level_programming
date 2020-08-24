aca2f9cf4a57b6d5db02fc0a3284fe2173dae8e7
mahdizaabi <mahdi.zaabi@gmail.com>
2020-08-24 02:45:21 +0000

    adding_file

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-#displays the value of a specific header field of the response.
+"""
+displays the value of a specific header field of the response.
+"""
 import urllib.request
 import sys
-
-url = sys.argv[1]
-req = urllib.request.Request(url)
-with urllib.request.urlopen(req) as response:
-    print(response.info()['X-Request-Id'])
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        print(response.info().get('X-Request-Id'))
