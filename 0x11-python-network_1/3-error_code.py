5c0809eea7660654749ccc96aac87badc4d99867
mahdizaabi <mahdi.zaabi@gmail.com>
2020-08-24 03:34:54 +0000

    adding_file

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,11 +4,11 @@ sends a request to the URL and displays the body of the response
 """
 import urllib.request
 import sys
-if __main__ == "__name__":
+if __name__ == "__main__":
     url = sys.argv[1]
     req = urllib.request.Request(url)
     try:
         with urllib.request.urlopen(req) as response:
-            print(response.read())
+            print(response.read().decode())
     except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
