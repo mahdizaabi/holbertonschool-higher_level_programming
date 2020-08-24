197815ab87855ac743d4b3790bd2d9b93bf518d8
mahdizaabi <mahdi.zaabi@gmail.com>
2020-08-24 05:13:35 +0000

    adding_file

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Send a POST request with a letter a s a parameter
+"""
+import requests
+import sys
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    if len(sys.argv) != 2:
+        r = requests.post(url, data={'q': ''})
+    else:
+        r = requests.post(url, data={'q': sys.argv[1]})
+
+    if r.json() and r.headers.get('content-type') == 'application/json':
+        id = r.json().get('id')
+        name = r.json().get('name')
+        print("[{}] {}".format(id, name))
+    elif r.headers.get('content-type') != "application/json":
+        print("Not a valid JSON")
+    else:
+        print("No result")
