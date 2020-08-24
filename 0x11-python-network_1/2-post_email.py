9c8e6d3f46e3ae801243f9cec01727adcf24bf74
mahdizaabi <mahdi.zaabi@gmail.com>
2020-08-24 03:03:57 +0000

    adding_file

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+sends a POST request to the passed URL with the email as a parameter
+"""
+import urllib.request
+import urllib.parse
+import sys
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email_adr = sys.argv[2]
+
+    values = {'email': 'email_adr'}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('utf8')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as reponse:
+        print(response.info())
