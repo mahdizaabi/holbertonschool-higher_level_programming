e4a4350934177ed52fe5a5b8708ddb6a6d9ff185
mahdizaabi <mahdi.zaabi@gmail.com>
2020-09-21 22:32:11 +0000

    add

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
-  for (let i of list) {
+  for (const i of list) {
     if (i === searchElement) {
       count++;
     }
