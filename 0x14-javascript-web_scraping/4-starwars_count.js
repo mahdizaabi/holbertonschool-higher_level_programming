8ba3b649a57e3a64de5628a4816bc5378d652792
mahdizaabi <mahdi.zaabi@gmail.com>
2020-09-22 12:28:43 +0000

    add

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,8 +6,8 @@ const ar = [];
 const url = process.argv[2];
 request(url, function (err, response, body) {
   if (err) { console.error('error:', err); }
-  for (const i of JSON.parse(body).results) {
-    ar.push(i.characters);
+  for (const c of JSON.parse(body).results) {
+    ar.push(c.characters);
   }
   for (let i = 0, len = ar.length; i < len; i++) {
     for (let j = 0, len2 = ar[i].length; j < len2; j++) {
