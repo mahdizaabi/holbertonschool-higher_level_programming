cb4663ad8e7a38c3b18f4f9679a0abcf0412c4f4
mahdizaabi <mahdi.zaabi@gmail.com>
2020-09-22 21:12:51 +0000

    add

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -5,16 +5,26 @@ const endpoint = 'https://swapi-api.hbtn.io/api/films/';
 const filter = process.argv[2];
 const url = endpoint + filter;
 const listx = [];
+const listname = [];
+let count = 0;
 request(url, function (error, response, body) {
   if (error) { return console.log(error); } else {
-    for (const star of JSON.parse(body).characters) {
-      listx.push(star);
+    for (let m = 0; m < JSON.parse(body).characters.length; m++) {
+      listx.push(JSON.parse(body).characters[m]);
     }
   }
   for (let k = 0; k < listx.length; k++) {
     request(listx[k], function (e, r, b) {
       if (e) { return console.log(e); }
-      console.log(JSON.parse(b).name);
+      listname.push(JSON.parse(b).name);
+      // console.log(JSON.parse(b).name);
+      // console.log(listname);
+      count++;
+      if (count === listx.length) {
+        for (let x = 0; x < listname.length; x++) { console.log(listname[x]); }
+      }
+    // console.log(listname);}
     });
+    // console.log(listname);
   }
 });
