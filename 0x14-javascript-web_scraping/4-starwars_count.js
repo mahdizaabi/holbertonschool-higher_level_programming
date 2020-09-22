9ad326e9a708d6e97a1cfa99985668ec5913bed7
mahdizaabi <mahdi.zaabi@gmail.com>
2020-09-22 12:26:45 +0000

    add

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -9,9 +9,8 @@ request(url, function (err, response, body) {
   for (const i of JSON.parse(body).results) {
     ar.push(i.characters);
   }
-  for (var i = 0, len = ar.length; i < len; i++) {
-    // inner loop applies to sub-arrays
-    for (var j = 0, len2 = ar[i].length; j < len2; j++) {
+  for (let i = 0, len = ar.length; i < len; i++) {
+    for (let j = 0, len2 = ar[i].length; j < len2; j++) {
       if (ar[i][j] === 'https://swapi-api.hbtn.io/api/people/18/') { count++; }
     }
   }
