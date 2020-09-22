56166db6e0d5eae30d631a062dcd2722b4066a2f
mahdizaabi <mahdi.zaabi@gmail.com>
2020-09-22 13:09:47 +0000

    add

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 
 const process = require('process');
+const file = process.argv[2];
 var fs = require('fs');
-fs.readFile(process.argv.splice(2)[0], 'utf-8', (err, data) => {
+fs.readFile(file, 'utf-8', (err, data) => {
   if (err) { console.log(err); }
   console.log(data);
 });
