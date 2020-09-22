25be315eca11d1e8b4f0e5da9d1361bdc1edb831
mahdizaabi <mahdi.zaabi@gmail.com>
2020-09-22 19:16:57 +0000

    ad

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -9,9 +9,7 @@ request(url, function (err, response, body) {
       if (i.completed === true) {
         if (done[i.userId] === undefined) {
           done[i.userId] = 1;
-        } else {
-          done[i.userId] = done[i.userId] + 1;
-        }
+        } else { done[i.userId] = done[i.userId] + 1; }
       }
     }
     console.log(done);
